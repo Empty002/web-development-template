804d7fb60ad5e81bb4a05424ef2eb9fc6a15c71d
Empty002 <realempty4@gmail.com>
2026-04-24 18:25:52 +0300

    Create the base folders and files, finish the template

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "development",
+
+  devtool: "inline-source-map",
+
+  devServer: {
+    static: "./dist",
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+});
